95062df3b5afcf53debec6b8169a8d5c89f9da81
Di Depan Layar <albinmaulana@yahoo.com>
2023-10-04 01:53:29 +0700

    Feature: Set Number Function

--- footer.php
+++ footer.php
@@ -38,6 +38,14 @@
                 });
             }
         });
+
+        function setNumber(evt) {
+            var charCode = (evt.which) ? evt.which : event.keyCode
+            if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+                return false;
+                return true;
+            }
+        }
     </script>
 </body>
 </html>
