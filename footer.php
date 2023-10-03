d2108c13bc6670ea3622117ea6c263a6950e4fcb
Di Depan Layar <albinmaulana@yahoo.com>
2023-10-04 01:35:02 +0700

    Refactor: Improve code on Include Function

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
         </div>
     </div>
     <!-- Javascripts -->
-    <script src="assets/plugins/jquery/jquery-3.4.1.min.js"></script>
+    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/jquery/jquery-3.4.1.min.js"></script>
     <script src="https://unpkg.com/@popperjs/core@2"></script>
-    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://unpkg.com/feather-icons"></script>
-    <script src="assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
-    <script src="assets/plugins/pace/pace.min.js"></script>
-    <script src="assets/js/main.min.js"></script>
+    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
+    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/pace/pace.min.js"></script>
+    <script src="<?php echo $data['siteurl']; ?>/assets/js/main.min.js"></script>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             const closeButton = document.querySelector('.btn-close');
@@ -20,21 +20,22 @@
                     }
                 });
             }
-        });
-    </script>
-    <script>
-        var inputAuthentication = document.getElementById('authentication');
-        var btnChangePassword = document.getElementById('btn-change-password');
 
-        btnChangePassword.addEventListener('click', function() {
-            if(inputAuthentication.disabled === true) {
-                inputAuthentication.removeAttribute('disabled');
-                inputAuthentication.focus();
-                btnChangePassword.innerText = 'Cancel';
-            } else {
-                inputAuthentication.setAttribute('disabled', 'disabled');
-                btnChangePassword.innerText = 'Change Password';
-                inputAuthentication.value = '';
+            const inputAuthentication = document.getElementById('authentication');
+            const btnChangePassword = document.getElementById('btn-change-password');
+
+            if (btnChangePassword) {
+                btnChangePassword.addEventListener('click', function() {
+                    if (inputAuthentication.disabled === true) {
+                        inputAuthentication.removeAttribute('disabled');
+                        inputAuthentication.focus();
+                        btnChangePassword.innerText = 'Cancel';
+                    } else {
+                        inputAuthentication.setAttribute('disabled', 'disabled');
+                        btnChangePassword.innerText = 'Change Password';
+                        inputAuthentication.value = '';
+                    }
+                });
             }
         });
     </script>
