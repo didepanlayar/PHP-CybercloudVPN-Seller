27ea240f4f9b70d77a4ad0a9b147f34f08cfb702
Di Depan Layar <albinmaulana@yahoo.com>
2023-10-06 21:59:43 +0700

    Refactor: Default permalink

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
         </div>
     </div>
     <!-- Javascripts -->
-    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/jquery/jquery-3.4.1.min.js"></script>
+    <script src="assets/plugins/jquery/jquery-3.4.1.min.js"></script>
     <script src="https://unpkg.com/@popperjs/core@2"></script>
-    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://unpkg.com/feather-icons"></script>
-    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
-    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/pace/pace.min.js"></script>
-    <script src="<?php echo $data['siteurl']; ?>/assets/plugins/DataTables/datatables.min.js"></script>
-    <script src="<?php echo $data['siteurl']; ?>/assets/js/main.min.js"></script>
-    <script src="<?php echo $data['siteurl']; ?>/assets/js/custom.js"></script>
+    <script src="assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
+    <script src="assets/plugins/pace/pace.min.js"></script>
+    <script src="assets/plugins/DataTables/datatables.min.js"></script>
+    <script src="assets/js/main.min.js"></script>
+    <script src="assets/js/custom.js"></script>
 </body>
 </html>
